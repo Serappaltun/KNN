1e23b4b6c25c027dae64ed2d9ea4ae7d41c5f3fc
serappaltun <32043039+paltun@users.noreply.github.com>
2018-10-10 19:50:05 +0300

    Initial commit

---
+++ KNN/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace KNN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxSinif = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Yas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Gelir = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sinif = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonEkle = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxK = new System.Windows.Forms.TextBox();
+            this.BtnHesapla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.textBoxHY = new System.Windows.Forms.TextBox();
+            this.textBoxHG = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Yaş:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gelir:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Sınıf:";
+            // 
+            // comboBoxSinif
+            // 
+            this.comboBoxSinif.FormattingEnabled = true;
+            this.comboBoxSinif.Location = new System.Drawing.Point(60, 104);
+            this.comboBoxSinif.Name = "comboBoxSinif";
+            this.comboBoxSinif.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxSinif.TabIndex = 6;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.Yas,
+            this.Gelir,
+            this.Sinif});
+            this.dataGridView1.Location = new System.Drawing.Point(232, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(482, 150);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "ID";
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
+            // Yas
+            // 
+            this.Yas.DataPropertyName = "YAS";
+            this.Yas.HeaderText = "Yas";
+            this.Yas.Name = "Yas";
+            // 
+            // Gelir
+            // 
+            this.Gelir.DataPropertyName = "GELIR";
+            this.Gelir.HeaderText = "Gelir";
+            this.Gelir.Name = "Gelir";
+            // 
+            // Sinif
+            // 
+            this.Sinif.DataPropertyName = "SINIF";
+            this.Sinif.HeaderText = "Sinif";
+            this.Sinif.Name = "Sinif";
+            // 
+            // buttonEkle
+            // 
+            this.buttonEkle.Location = new System.Drawing.Point(12, 144);
+            this.buttonEkle.Name = "buttonEkle";
+            this.buttonEkle.Size = new System.Drawing.Size(75, 23);
+            this.buttonEkle.TabIndex = 8;
+            this.buttonEkle.Text = "Ekle";
+            this.buttonEkle.UseVisualStyleBackColor = true;
+            this.buttonEkle.Click += new System.EventHandler(this.buttonEkle_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(60, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBoxK
+            // 
+            this.textBoxK.Location = new System.Drawing.Point(40, 214);
+            this.textBoxK.Name = "textBoxK";
+            this.textBoxK.Size = new System.Drawing.Size(100, 20);
+            this.textBoxK.TabIndex = 11;
+            // 
+            // BtnHesapla
+            // 
+            this.BtnHesapla.Location = new System.Drawing.Point(40, 332);
+            this.BtnHesapla.Name = "BtnHesapla";
+            this.BtnHesapla.Size = new System.Drawing.Size(75, 23);
+            this.BtnHesapla.TabIndex = 12;
+            this.BtnHesapla.Text = "Hesapla";
+            this.BtnHesapla.UseVisualStyleBackColor = true;
+            this.BtnHesapla.Click += new System.EventHandler(this.BtnHesapla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "K:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(232, 214);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 14;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(379, 214);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 15;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(534, 214);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 95);
+            this.listBox3.TabIndex = 16;
+            // 
+            // textBoxHY
+            // 
+            this.textBoxHY.Location = new System.Drawing.Point(40, 251);
+            this.textBoxHY.Name = "textBoxHY";
+            this.textBoxHY.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHY.TabIndex = 17;
+            // 
+            // textBoxHG
+            // 
+            this.textBoxHG.Location = new System.Drawing.Point(40, 288);
+            this.textBoxHG.Name = "textBoxHG";
+            this.textBoxHG.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHG.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 258);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Yaş:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 291);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Gelir:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(232, 195);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Uzaklıklar (A)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(376, 195);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Uzaklıklar (B)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(531, 195);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(38, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Sonuç";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 382);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxHG);
+            this.Controls.Add(this.textBoxHY);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnHesapla);
+            this.Controls.Add(this.textBoxK);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonEkle);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBoxSinif);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxSinif;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonEkle;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxK;
+        private System.Windows.Forms.Button BtnHesapla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.TextBox textBoxHY;
+        private System.Windows.Forms.TextBox textBoxHG;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Yas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Gelir;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sinif;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ KNN/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KNN
+{
+    public partial class Form1 : Form
+    {
+        string[] snf = { "A", "B" };
+        private BindingSource bindingSource1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            initApp();
+        }
+
+        private void initApp()
+        {
+            bindingSource1 = new BindingSource();
+            dataGridView1.DataSource = bindingSource1;
+            listBox1.DisplayMember = "uzaklikDeger";
+            listBox2.DisplayMember = "uzaklikDeger";
+            for (int i = 0; i < 4; i++)
+            {
+
+                bindingSource1.Add(new Kisi(bindingSource1, 20 + i, 20 + 100 * i, snf[(i + 2) % 2]));
+                dataGridView1.DataSource = bindingSource1;
+            }
+        }
+
+        private void buttonEkle_Click(object sender, EventArgs e)
+        {
+            int yas= Convert.ToInt32(textBox1.Text);
+            int gelir=Convert.ToInt32( textBox2.Text);
+            string sinif= comboBoxSinif.SelectedItem.ToString();
+            bindingSource1.Add(new Kisi(bindingSource1, yas, gelir, sinif));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxSinif.Items.AddRange(snf);
+        }
+
+        private void BtnHesapla_Click(object sender, EventArgs e)
+        {
+
+            int yeni_yas= Convert.ToInt32(textBoxHY.Text);
+            int yeni_gelir =Convert.ToInt32(textBoxHG.Text);
+            int k = Convert.ToInt32(textBoxK.Text);
+
+            KisiUzaklik[] k_nn= new KisiUzaklik[k];
+
+            List<KisiUzaklik> uzakliklarA = new List<KisiUzaklik>();
+            List<KisiUzaklik> uzakliklarB = new List<KisiUzaklik>();
+            double satir = dataGridView1.RowCount - 1;
+            for (int i = 0; i < satir; i++)
+            {
+                int id= Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value);
+                int yas= Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value);
+                int gelir= Convert.ToInt32(dataGridView1.Rows[i].Cells[2].Value);
+                string sinif= dataGridView1.Rows[i].Cells[3].Value.ToString();
+                double x= yas-yeni_yas;
+                double y= gelir-yeni_gelir;
+
+                double uzaklik= Math.Sqrt(Math.Pow(x,2)+Math.Pow(y,2));
+
+                KisiUzaklik ks = new KisiUzaklik(id, uzaklik, sinif);
+
+                diziyeEkle(k_nn,ks);
+
+                if (sinif.Equals(snf[0]))
+                {
+                    uzakliklarA.Add(ks);
+                }
+                else
+                {
+                    uzakliklarB.Add(ks);
+                }
+            }
+
+            string yeni_sinif = uzaklikAnalizEt(k_nn);
+
+            listBox1.DataSource = uzakliklarA;
+            listBox2.DataSource = uzakliklarB;
+            listBox3.Items.Add("sinif: "+yeni_sinif);
+        }
+
+        private void diziyeEkle(KisiUzaklik[] k_nn,KisiUzaklik ks)
+        {
+            for (int i = 0; i < k_nn.Length; i++)
+            {
+                if (k_nn[i] == null)
+                {
+                    k_nn[i] = ks;
+                    return;
+                }
+            }
+
+            int max_index=0;
+            double max_uzaklik = k_nn[max_index].uzaklikDouble;
+            for (int i = 1; i < k_nn.Length; i++)
+            {
+                if (k_nn[i].uzaklikDouble > max_uzaklik)
+                {
+                    max_uzaklik = k_nn[i].uzaklikDouble;
+                    max_index = i;
+                }
+            }
+            if (max_uzaklik > ks.uzaklikDouble)
+            {
+                k_nn[max_index] = ks;
+            }
+            return;
+        }
+
+        private string uzaklikAnalizEt(KisiUzaklik[] k_nn)
+        {
+            int[] analiz = new int[2];
+            analiz[0]=0;
+            analiz[1]=0;
+
+            for (int i = 0; i<k_nn.Length; i++)
+            {
+                if (k_nn[i].snf.Equals(snf[0]))
+                {
+                    analiz[0] = analiz[0]+1;
+                }
+                else
+                {
+                    analiz[1] = analiz[1] + 1;
+                }
+            }
+
+            if (analiz[0] > analiz[1])
+            {
+                return snf[0];
+            }
+            else
+            {
+                return snf[1];
+            }
+        }
+
+        #region "kisi object"
+        private class Kisi
+        {
+            private int yas;
+            private int id;
+            private int gelir;
+            private String sinif;
+
+            public Kisi(BindingSource bindingSource, int yas,int gelir, string sinif)
+            {
+                this.id = bindingSource.Count;
+                this.yas=yas;
+                this.gelir=gelir;
+                this.sinif=sinif;
+
+            }
+
+            public Kisi()
+            {
+
+            }
+
+            public int ID
+            {
+                get
+                {
+                    return id;
+                }
+
+                set
+                {
+                    id = value;
+                }
+            }
+
+            public int YAS
+            {
+                get
+                {
+                    return yas;
+                }
+
+                set
+                {
+                    yas = value;
+                }
+            }
+
+            public int GELIR
+            {
+                get
+                {
+                    return gelir;
+                }
+                set
+                {
+                    gelir = value;
+                }
+            }
+
+            public string SINIF
+            {
+                get
+                {
+                    return sinif;
+                }
+                set
+                {
+                    sinif = value;
+                }
+            }
+        }
+        #endregion
+
+        #region "KisiUzaklik object"
+        private class KisiUzaklik
+        {
+            private int id;
+            private double uzaklik;
+            private string sinif;
+
+            public KisiUzaklik(int id, double uzaklik, string sinif)
+            {
+                this.id = id;
+                this.uzaklik = uzaklik;
+                this.sinif = sinif;
+            }
+
+            public KisiUzaklik()
+            {
+            }
+
+            public int ID
+            {
+                get
+                {
+                    return id;
+                }
+
+                set
+                {
+                    id = value;
+                }
+            }
+
+            public string uzaklikDeger
+            {
+                get
+                {
+                    return id+": "+uzaklik;
+                }
+                
+            }
+
+            public double uzaklikDouble
+            {
+                get
+                {
+                    return uzaklik;
+                }
+
+            }
+
+            public string snf
+            {
+                get
+                {
+                    return sinif;
+                }
+            }
+        }
+        #endregion
+
+    }
+
+    
+}
